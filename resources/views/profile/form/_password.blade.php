ece29f90e261693ab3ac0183b8d45383c06cd31b
Gerardo Belot <gbelot2003@hotmail.com>
2020-08-30 11:39:34 -0600

    refactory _password.blade con valores correctos para confirmación

--- resources/views/profile/form/_password.blade.php
+++ resources/views/profile/form/_password.blade.php
@@ -1,22 +1,16 @@
 <fieldset>
     <legend>{{ __('Change your password') }}</legend>
     <div class="row">
-        <div class="col-md-12">
-            <div class="form-group text-left">
-                <label for="current_password">Current Password</label>
-                <input type="password" name="current_password" id="current_password" class="form-control">
-            </div>
-        </div>
         <div class="col-md-6">
             <div class="form-group text-left">
                 <label for="new_password">New Password</label>
-                <input type="password" name="new_password" id="new_password" class="form-control">
+                <input type="password" name="password" id="new_password" class="form-control">
             </div>
         </div>
         <div class="col-md-6">
             <div class="form-group text-left">
-                <label for="Confirm_password">Confirm Password</label>
-                <input type="password" name="Confirm_password" id="Confirm_password" class="form-control">
+                <label for="password_confirmation">Confirm Password</label>
+                <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
             </div>
         </div>
         <div class="col-md-12 text-left">
